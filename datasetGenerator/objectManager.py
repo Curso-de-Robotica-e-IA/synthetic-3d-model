4d455d4a88049abe128310f27e6ba38c8ae76da2
diasjulia <jedl@softex.cin.ufpe.br>
2025-06-04 20:21:22 -0300

    feat: movement adjustments

--- datasetGenerator/objectManager.py
+++ datasetGenerator/objectManager.py
@@ -12,10 +12,8 @@ class ObjectManager:
         obj1, obj2 = objects
 
         angle_rad = math.radians(angle_degrees)
-        half_angle = angle_rad / 2
         
-        obj1.rotation_euler.z = -half_angle
-        obj2.rotation_euler.z = half_angle
+        obj1.rotation_euler.z = -angle_rad
     
     def flip_objects(self):
         if not self.flipped:
@@ -35,12 +33,27 @@ class ObjectManager:
     def move_objects(self, location):
         for obj in self.objects:
             obj.location = location
+    
+    def randomly_move_object(self, objects, x_range=(-0.3, 0.3), y_range=(-0.3, 0.3)):
+        x = random.uniform(*x_range)
+        y = random.uniform(*y_range)
+        for obj in objects:
+            # Move the object to a new random position
+            obj.location = Vector((x, y, obj.location.z))
+        return
             
     def reset_objects(self):
         for obj in self.objects:
             obj.rotation_euler = (0, 0, 0)
             obj.scale = (1, 1, 1)
+            obj.hide_set(False)
         self.flipped = False
+    
+    def ocult_objects(self, objects=None):
+        if objects is None:
+            objects = self.objects
+        for obj in objects:
+            obj.hide_set(True)
     
     def return_objects(self):
         return self.objects
